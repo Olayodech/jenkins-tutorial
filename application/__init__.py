a7481ab72ee60bd4ae8b3e9cef02e31b612a98b0
olayodech <olatunjicharles1@gmail.com>
2022-07-24 22:02:01 +0100

    added flask blueprints, materialize and restructure folder

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from flask_material import Material
+from application.routes.home import bp as home_bp
 
 app = Flask(__name__)
+Material(app)
 
+app.register_blueprint(home_bp)
 from application import routes
---
+++ application/routes/home/route.py
@@ -0,0 +1,27 @@
+from flask import render_template
+from application.routes.home import bp
+
+
+# @app.route('/')
+# @app.route('/index')
+@bp.route('/')
+@bp.route('/index')
+def getHome():
+    user = {'username': 'Charles'}
+    items = [
+        {
+            'name': 'Dior',
+            'price': 5000,
+            'discount': 0,
+            'description': 'Dior exclesior, branded to suit you',
+            'image': "dior.png"
+        },
+        {
+            'name': 'LV',
+            'price': 13000,
+            'discount': 0,
+            'description': 'LV deluxe, branded to suit you',
+            'image': "lv.png"
+        }
+    ]
+    return render_template('/home/index.html', title='Home', user=user, items=items)
